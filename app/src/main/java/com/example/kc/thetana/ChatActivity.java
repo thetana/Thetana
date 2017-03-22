3abd9ca573b3637581a08734b91cd1dbc3bd1ca6
thetana <zxsa135@naver.com>
2017-03-22 22:40:53 +0900

    2017-03-22

--- app/src/main/java/com/example/kc/thetana/ChatActivity.java
+++ app/src/main/java/com/example/kc/thetana/ChatActivity.java
@@ -2,24 +2,10 @@ package com.example.kc.thetana;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.net.URI;
 
 /**
  * Created by kc on 2017-02-25.
@@ -27,6 +13,7 @@ import java.net.URI;
 
 public class ChatActivity extends ActionBarActivity {
     String imgDecodableString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,15 +45,13 @@ public class ChatActivity extends ActionBarActivity {
 //        }
 //    }
 
-    private void openGallery()
-    {
+    private void openGallery() {
         Intent galleryintent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryintent, 1);
     }
 
     @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == RESULT_OK
                 && null != data) {
